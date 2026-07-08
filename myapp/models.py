b89d8fe90641da6016ecc8e79bd7fd9adb3eb67e
LAB22_USER ON WS18 <L22X13W18@LabHCI.net>
2026-07-08 13:29:12 +0800

    Added Loan model

--- myapp/models.py
+++ myapp/models.py
@@ -21,3 +21,21 @@ class Laptop(models.Model):
 
     def __str__(self):
         return f"{self.asset_tag} - {self.brand} {self.model}"
+
+class Loan(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('returned', 'Returned'),
+        ('overdue', 'Overdue'),
+    ]
+
+    laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, related_name='loans')
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrower')
+    processed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='loans_processed')
+    checkout_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    return_date = models.DateTimeField(null=True, blank=True)
+    loan_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        return f"{self.laptop.asset_tag} - {self.borrower.username} {self.loan_status}"
