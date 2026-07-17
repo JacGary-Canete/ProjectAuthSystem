497d1011dbbe6fcb36693aa68b455c39a3e4a9f5
LAB22_USER ON WS18 <L22X13W18@LabHCI.net>
2026-07-17 14:55:07 +0800

    Redirect empty root path to login page

--- project_auth_system/urls.py
+++ project_auth_system/urls.py
@@ -3,8 +3,10 @@ URL configuration for project_auth_system project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView # Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    path('', RedirectView.as_view(pattern_name='login'), name='root_redirect'),
 ]
